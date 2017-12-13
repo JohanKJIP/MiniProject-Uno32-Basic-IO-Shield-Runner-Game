4205287a3ea1ea8b769fa184e701b9d4e4f686e6
David Johansson <davidjo2@kth.se>
2017-12-13 09:43:07 +0100

    added output pins for display signal

--- src/main.c
+++ src/main.c
@@ -18,12 +18,29 @@ int SCORE = 0;
 int main(void) {
     /* === Code from lab */
 	/* Output pins for display signals */
+    SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
+    SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
+    while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
+    OSCCONCLR = 0x180000; /* clear PBDIV bit <0,1> */
+    while(OSCCON & (1 << 21));  /* Wait until PBDIV ready */
+    SYSKEY = 0x0;  /* Lock OSCCON */
+
+    /* Set up output pins */
+    AD1PCFG = 0xFFFF;
+    ODCE = 0x0;
+    TRISECLR = 0xFF;
+    PORTE = 0x0;
+
 	PORTF = 0xFFFF;
 	PORTG = (1 << 9);
 	ODCF = 0x0;
 	ODCG = 0x0;
 	TRISFCLR = 0x70;
 	TRISGCLR = 0x200;
+
+    /* Set up input pins */
+	TRISDSET = (1 << 8);
+	TRISFSET = (1 << 1);
 
 	/* Set up SPI as master */
 	SPI2CON = 0;
